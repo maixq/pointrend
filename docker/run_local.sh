bd43928de2d163c2f13a33a2f3a19153aef41800
maixq <mai.xueqiao@carro.co>
2023-02-23 10:04:15 +0000

    added arg

--- docker/run_local.sh
+++ docker/run_local.sh
@@ -2,8 +2,12 @@
 IMAGE=pointrend
 
 docker run \
+--rm \
 --interactive \
+--shm-size 8G \
 --name pointrend-container \
 -v $(pwd)/data:/pointrend/data \
-
-$IMAGE
+--gpus all \
+$IMAGE \
+--config-file /pointrend/data/dent/run3/configs/pointrend_rcnn_R_50_FPN_1x_coco.yaml \
+# --eval-only
