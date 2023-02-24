f97fc2ac634e28197ca7781733fc3e048c8fd9d2
maixq <mai.xueqiao@carro.co>
2023-02-24 08:45:47 +0000

    change run name

--- register_dataset.py
+++ register_dataset.py
@@ -1,31 +1,29 @@
 
 from detectron2.data.datasets import register_coco_instances
 
-def register_damage_detection():
-    DATASET = 'run_1'
+def register_damage_detection(run):
+    DATASET = run
     
     # train
     register_coco_instances(
         "{}_train".format(DATASET),
         {},
-        "/pointrend/data/dent/{}/annotations/train.json".format(DATASET),
-        "/pointrend/data/dent/{}/images/train/".format(DATASET),
+        "/pointrend/data/{}/annotations/train.json".format(DATASET),
+        "/pointrend/data/{}/images/train/".format(DATASET),
     )
     
     # val
     register_coco_instances(
         "{}_val".format(DATASET),
         {},
-        "/pointrend/data/dent/{}/annotations/val.json".format(DATASET),
-        "/pointrend/data/dent/{}/images/val/".format(DATASET),
+        "/pointrend/data/{}/annotations/val.json".format(DATASET),
+        "/pointrend/data/{}/images/val/".format(DATASET),
     )
     
     # test
     register_coco_instances(
         "{}_test".format(DATASET),
         {},
-        "/pointrend/data/dent/{}/annotations/test.json".format(DATASET),
-        "/pointrend/data/dent/{}/images/test/".format(DATASET),
+        "/pointrend/data/{}/annotations/test.json".format(DATASET),
+        "/pointrend/data/{}/images/test/".format(DATASET),
     )
-    
-    
