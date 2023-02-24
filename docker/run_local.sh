660ed2fbdd969c111aa7de07e109d6a11e514f6a
maixq <mai.xueqiao@carro.co>
2023-02-24 08:46:23 +0000

    increase shm size

--- docker/run_local.sh
+++ docker/run_local.sh
@@ -1,13 +1,15 @@
 
 IMAGE=pointrend
+RUN=dent_run1
 
 docker run \
---rm \
 --interactive \
---shm-size 8G \
 --name pointrend-container \
 -v $(pwd)/data:/pointrend/data \
+--runtime=nvidia \
+--shm-size 8G \
 --gpus all \
-$IMAGE \
---config-file /pointrend/data/dent/run3/configs/pointrend_rcnn_R_50_FPN_1x_coco.yaml \
+$IMAGE 
+--config-file /pointrend/data/$RUN/configs/pointrend_rcnn_R_50_FPN_1x_coco.yaml \
 # --eval-only
+# --resume
