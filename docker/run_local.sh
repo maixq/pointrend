420d73da1b2e9e187d2e16016d4dbc3e8b37011f
maixq <mai.xueqiao@carro.co>
2023-02-27 03:01:31 +0000

    add args during docker run

--- docker/run_local.sh
+++ docker/run_local.sh
@@ -1,15 +1,18 @@
 
 IMAGE=pointrend
 RUN=dent_run1
+MODEL_WEIGHTS=/pointrend/data/$RUN/output/model_final_run3.pth
+CONFIG=/pointrend/data/$RUN/configs/pointrend_rcnn_R_50_FPN_1x_coco.yaml
 
 docker run \
 --interactive \
---name pointrend-container \
 -v $(pwd)/data:/pointrend/data \
 --runtime=nvidia \
 --shm-size 8G \
 --gpus all \
-$IMAGE 
---config-file /pointrend/data/$RUN/configs/pointrend_rcnn_R_50_FPN_1x_coco.yaml \
-# --eval-only
+$IMAGE \
+--config-file \
+$CONFIG \
+--eval-only \
+MODEL.WEIGHTS $MODEL_WEIGHTS
 # --resume
